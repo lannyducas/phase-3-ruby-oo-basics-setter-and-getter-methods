24c65c207b5694fc3746ff745e1735f4ba66d986
Danny Lucas <lannyducas@gmail.com>
2022-08-31 15:44:28 -0400

    commit tree dcfcfbaace5efbd3254c81bd2ea3edb0a1f2ff13

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
-class Person
 
-end
+class Person
+    def name= (str)
+        @name = str
+    end
+
+    def name
+      @name
+    end
+  
+    def job=(str)
+      @job = str
+    end
+    def job
+      @job
+    end
+  
+  end
